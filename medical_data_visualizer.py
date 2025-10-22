f14e08d8231f130abc8201a9a326fb7a537d94a8
Shiva Kumar <kurvashiva04@gmail.com>
2025-10-22 12:06:00 +0530

    Made final changes

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,60 +1,34 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import numpy as np
 
-# 1
-df = None
-
-# 2
-df['overweight'] = None
-
-# 3
-
-
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    df = pd.read_csv('medical_examination.csv')
+    df['overweight'] = (df['weight'] / (df['height'] / 100)**2 > 25).astype(int)
+    df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+    df['gluc'] = (df['gluc'] > 1).astype(int)
 
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
-    # 6
-    df_cat = None
-    
-
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    fig = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar').fig
     fig.savefig('catplot.png')
     return fig
 
-
-# 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+    df = pd.read_csv('medical_examination.csv')
+    df['overweight'] = (df['weight'] / (df['height'] / 100)**2 > 25).astype(int)
+    df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+    df['gluc'] = (df['gluc'] > 1).astype(int)
 
-    # 12
-    corr = None
+    df = df[df['ap_lo'] <= df['ap_hi']]
+    df = df[df['height'].between(df['height'].quantile(0.025), df['height'].quantile(0.975))]
+    df = df[df['weight'].between(df['weight'].quantile(0.025), df['weight'].quantile(0.975))]
 
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
+    corr = df.corr()
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+    fig, ax = plt.subplots(figsize=(12, 10))
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", center=0, linewidths=.5, square=True, cbar_kws={"shrink": .45})
     fig.savefig('heatmap.png')
     return fig
